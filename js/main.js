7e58ea1cb6ed98c25d302b830a3d73a141ebdef2
Datario <74454947+datario-viz@users.noreply.github.com>
2021-01-19 18:16:43 +0800

    Website go!

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// Open / close side nav
+var sideNav = document.getElementById("sideNav");
+function openSideNav() {
+    sideNav.style.width = "250px";
+    sideNav.style.borderLeft = "4px solid var(--white-0)";
+}
+
+function closeSideNav() {
+    sideNav.style.width = "0";
+    sideNav.style.borderLeft = "0px solid var(--white-0)";
+}
+
+// Hide header when scrolling down
+var prevScroll = window.pageYOffset;
+window.onscroll = function() {
+    var crntScroll = window.pageYOffset;
+    var topNav = document.getElementById("topNav");
+    if (prevScroll > crntScroll) {
+        topNav.style.top = "0";
+    } else {
+        topNav.style.top = "-68px";
+    }
+    prevScroll = crntScroll;
+}
+
+// Toggle accordions
+var acc = document.getElementsByClassName("accordion");
+for (var el = 0; el < acc.length; el++) {
+    acc[el].addEventListener("click", function() { 
+        // Accordion buttons' state toggle
+        if (this.className === "accordion") {
+            this.className += " acc-on";
+        } else {
+            this.className = "accordion";
+        }
+        // Panel toggle
+        var accTxt = this.nextElementSibling;
+        if (accTxt.className === "accordion-txt") {
+            accTxt.className += " acc-txt-on";
+        } else {
+            accTxt.className = "accordion-txt";
+        }
+    });
+}
